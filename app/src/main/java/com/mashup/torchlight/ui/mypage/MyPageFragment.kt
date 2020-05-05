578125abe560d9bd04aef225a22b6bd49cf9e868
dlwls5201 <dlwls5201@naver.com>
2020-05-06 07:17:47 +0900

    로그인 및 회원가입 임시 연결

--- app/src/main/java/com/mashup/torchlight/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/com/mashup/torchlight/ui/mypage/MyPageFragment.kt
@@ -1,19 +1,33 @@
 package com.mashup.torchlight.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import com.mashup.base.baseview.BaseFragment
 import com.mashup.torchlight.R
-import com.mashup.torchlight.databinding.FragmentNotificationsBinding
+import com.mashup.torchlight.databinding.FragmentMyPageBindingImpl
+import com.mashup.torchlight.ui.signin.SignInActivity
+import com.mashup.torchlight.ui.signup.SignUpActivity
+import kotlinx.android.synthetic.main.fragment_my_page.*
 
 class MyPageFragment :
-    BaseFragment<FragmentNotificationsBinding>(R.layout.fragment_my_page) {
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }
+    BaseFragment<FragmentMyPageBindingImpl>(R.layout.fragment_my_page) {
 
     companion object {
         fun newInstance() = MyPageFragment()
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnSignIn.setOnClickListener {
+            startActivity(
+                Intent(requireContext(), SignInActivity::class.java)
+            )
+        }
+        btnSignUp.setOnClickListener {
+            startActivity(
+                Intent(requireContext(), SignUpActivity::class.java)
+            )
+        }
+    }
 }
